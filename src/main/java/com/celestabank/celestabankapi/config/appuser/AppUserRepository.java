74d14af5adbf5a625c9223a3c5845ac646783c51
Yahnjou Piedjou Samuel <samuelbicombai@gmail.com>
2022-06-10 10:47:52 +0200

    ADD BEST PRACTICE

---
+++ src/main/java/com/celestabank/celestabankapi/config/appuser/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.celestabank.celestabankapi.config.appuser;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
+}
---
+++ src/main/java/com/celestabank/celestabankapi/config/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.celestabank.celestabankapi.config.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
